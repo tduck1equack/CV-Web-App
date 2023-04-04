1bde733571c89f16e284a8d0dc191b34b41c5cb5
DuckToFo <golden4gnvx@gmail.com>
2023-04-04 17:30:06 +0700

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,31 +5,31 @@ import "./Navbar.css"
 
 const Navbar = (props) => {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid dark-theme">
-            <a class="navbar-brand" href="#">{props.title}</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid dark-theme">
+            <a className="navbar-brand" href="#">{props.title}</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">{props.one}</a>
+    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul classNameName="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link" aria-current="page" href="#">{props.one}</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">{props.two}</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">{props.two}</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">{props.three}</a>
+        <li className="nav-item">
+          <a className="nav-link" href="#">{props.three}</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {props.dropdown}
           </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">{props.ddone}</a></li>
-            <li><a class="dropdown-item" href="#">{props.ddtwo}</a></li>
-            <li><a class="dropdown-item" href="#">{props.ddthree}</a></li>
+          <ul className="dropdown-menu">
+            <li><a className="dropdown-item" href="#">{props.ddone}</a></li>
+            <li><a className="dropdown-item" href="#">{props.ddtwo}</a></li>
+            <li><a className="dropdown-item" href="#">{props.ddthree}</a></li>
           </ul>
         </li>
       </ul>
